58effccfd26ba171c01c5b1795db1d6844b9a106
Dennizz <root@ipaddr.nl>
2021-12-03 11:50:03 +0100

    add example code

---
+++ examples/AWS Existing VPC Spoke/main.tf
@@ -0,0 +1,27 @@
+data "aws_vpc" "example" {
+  id = var.vpc_id
+}
+
+data "aws_subnet" "example_gw" {
+  id = var.gw_subnet_id
+}
+
+data "aws_subnet" "example_hagw" {
+  id = var.hagw_subnet_id
+}
+
+module "spoke_aws_1" {
+  source  = "../../"
+
+  cloud            = "AWS"
+  name             = "App1"
+  cidr             = "10.1.0.0/20"
+  region           = "eu-west-1"
+  account          = "AWS-Account"
+  transit_gw       = "avx-eu-west-1-transit"
+  security_domain  = "blue"
+  use_existing_vpc = true
+  vpc_id           = data.aws_vpc.example.vpc_id
+  gw_subnet        = data.aws_subnet.example_gw.cidr_block
+  hagw_subnet      = data.aws_subnet.example_hagw.cidr_block #Use separate subnets in multiple AZ's for gw and hagw, for multi AZ availability.
+}
