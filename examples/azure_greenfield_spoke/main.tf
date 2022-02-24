ef762b351c04042e5f60b14ba2443b42424939a5
Jo Scheuerer <joscheuerer@gmail.com>
2022-02-24 15:22:34 +0100

    change region on Azure Greenfield

--- examples/azure_greenfield_spoke/main.tf
+++ examples/azure_greenfield_spoke/main.tf
@@ -5,7 +5,7 @@ module "spoke_azure_1" {
   cloud           = "Azure"
   name            = "App1"
   cidr            = "10.1.0.0/20"
-  region          = "eu-west-1"
+  region          = "West Europe"
   account         = "Azure-Account"
   transit_gw      = "avx-west-europe-transit"
   security_domain = "green"
