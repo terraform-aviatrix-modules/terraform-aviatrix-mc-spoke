56003e46807f697037273543f7b5629adae39b89
Dennizz <root@ipaddr.nl>
2022-03-28 09:32:26 +0200

    5404 updates

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "aviatrixsystems/aviatrix"
-      version = "2.21.0-6.6.ga"
+      version = "2.21.1-6.6.ga"
     }
   }
   required_version = ">= 0.13"
