85e0e8f7c17f49144befd8959f3db1c0305f4e89
Dennizz <root@ipaddr.nl>
2022-10-04 08:51:59 +0200

    actually bump version constraint

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "aviatrixsystems/aviatrix"
-      version = "~> 2.23.0"
+      version = "~> 2.24.0"
     }
   }
   required_version = ">= 1.2.0"
