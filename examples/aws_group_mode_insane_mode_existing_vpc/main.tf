69d355add3df758b7e2f95be4a9c714d6b3bf7f1
ragaavx <rananth@aviatrix.com>
2023-10-05 21:06:54 +0100

    with tf fmt

--- examples/aws_group_mode_insane_mode_existing_vpc/main.tf
+++ examples/aws_group_mode_insane_mode_existing_vpc/main.tf
@@ -14,17 +14,17 @@ module "spoke_aws_1" {
   source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
   version = "1.6.5"
 
-  cloud                  = "AWS"
-  name                   = "awsapp1"
-  region                 = "us-east-1"
-  account                = "aws-account"
-  transit_gw             = "avx-us-east-1-tg"
-  vpc_id                 = data.aws_vpc.example.vpc_id
-  gw_subnet              = data.aws_subnet.example_gw.cidr_block
-  hagw_subnet            = data.aws_subnet.example_hagw.cidr_block #Use separate subnets in multiple AZ's for gw and hagw, for multi AZ availability.
-  insane_mode            = true
-  use_existing_vpc       = true
-  inspection             = true
+  cloud            = "AWS"
+  name             = "awsapp1"
+  region           = "us-east-1"
+  account          = "aws-account"
+  transit_gw       = "avx-us-east-1-tg"
+  vpc_id           = data.aws_vpc.example.vpc_id
+  gw_subnet        = data.aws_subnet.example_gw.cidr_block
+  hagw_subnet      = data.aws_subnet.example_hagw.cidr_block #Use separate subnets in multiple AZ's for gw and hagw, for multi AZ availability.
+  insane_mode      = true
+  use_existing_vpc = true
+  inspection       = true
 
   #Group mode settings 3 GW's, provide the additional subnet CIDR's for spoke gateways 3-n.
   group_mode                    = true
