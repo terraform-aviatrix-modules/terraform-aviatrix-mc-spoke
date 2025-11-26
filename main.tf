eb9c928a4951f1a27663dfb6276269ec9523d096
Dennizz <root@ipaddr.nl>
2025-11-26 12:15:46 +0100

    fix argument names

--- main.tf
+++ main.tf
@@ -68,8 +68,8 @@ resource "aviatrix_spoke_gateway" "default" {
   availability_domain                   = local.availability_domain
   fault_domain                          = local.fault_domain
   enable_active_standby                 = var.enable_active_standby
-  ph2_encryption_policy                 = var.tunnel_encryption_cipher
-  ph2_pfs_policy                        = var.tunnel_forward_secrecy
+  tunnel_encryption_cipher              = var.tunnel_encryption_cipher
+  tunnel_forward_secrecy                = var.tunnel_forward_secrecy
   prepend_as_path                       = var.prepend_as_path
   enable_learned_cidrs_approval         = var.enable_learned_cidrs_approval
   learned_cidrs_approval_mode           = var.learned_cidrs_approval_mode
